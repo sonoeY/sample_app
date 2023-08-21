a348798e235d6cb60043c096e631cd8431b6280a
Yama <sonoe2000yamaguchi@gmail.com>
2023-08-21 06:48:45 +0000

    Rails Tutorial 9.1 done

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -9,31 +9,28 @@ class SessionsController < ApplicationController
     user = User.find_by(email: params[:session][:email].downcase)
     if user && user.authenticate(params[:session][:password])
       # ユーザーログイン後にユーザー情報のページにリダイレクトする
-      #Success
-      #log_in(@user)
       
       reset_session      # ログインの直前に必ずこれを書くこと
+      remember user
       log_in user
       redirect_to user
       
     else
       #ログイン失敗時の処理(フラッシュメッセージ)
-      #Failure-----> alert-danger => 赤色のフラッシュ
       #.now -----> 次のリクエストが来たら消えてください
       flash.now[:danger] = 'Invalid email/password combination' 
       
       # エラーメッセージを作成する
       render 'new', status: :unprocessable_entity
       
-      #redirect_to vs. render
-      #GET/users/1 => show template
-      #               render 'now' (0回)
+      #redirect : GET/users/1 => show template
+      #render   :                render 'now' (0回)
     end
     
   end
 
   def destroy
-    log_out
+    log_out if logged_in?
     redirect_to root_url, status: :see_other
   end
   
