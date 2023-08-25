0fb3bc9fa382959f1b3165e1377d5c90526986ad
Yama <sonoe2000yamaguchi@gmail.com>
2023-08-25 02:41:50 +0000

    Add user microposts ---done bofore 13.4.4 AWS setting---

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,15 @@
 class ApplicationController < ActionController::Base
   #ApplicationコントローラにSessionヘルパーモジュールを読み込む
   include SessionsHelper
+  
+  private
+
+    # ユーザーのログインを確認する
+    def logged_in_user
+      unless logged_in?
+        store_location
+        flash[:danger] = "Please log in."
+        redirect_to login_url, status: :see_other
+      end
+    end
 end
